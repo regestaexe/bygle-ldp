b2192b3a65f2886b08fb5b06df083c695cf3b616
Diego Valerio Camarda <diego@lodlive.it>
2015-02-19 23:39:09 +0100

    added drop method

--- src/main/java/org/bygle/listeners/BygleListener.java
+++ src/main/java/org/bygle/listeners/BygleListener.java
@@ -16,36 +16,39 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-
 public class BygleListener implements ServletContextListener {
 	private static final Logger logger = LoggerFactory.getLogger(BygleListener.class);
-    public void contextDestroyed(ServletContextEvent arg0) {
-    	Enumeration<Driver> drivers = DriverManager.getDrivers();
-        Driver d = null;
-        while(drivers.hasMoreElements()) {
-            try {
-                d = drivers.nextElement();
-                DriverManager.deregisterDriver(d);
-                logger.warn(String.format("Driver %s deregistered", d));
-            } catch (SQLException ex) {
-            	logger.warn(String.format("Error deregistering driver %s", d), ex);
-            }
-        }
-    }
 
-    public void contextInitialized(ServletContextEvent arg0) {
-    	try {
-			if(BygleSystemUtils.getStringProperty("hibernate.hbm2ddl.auto").equals("create")){
+	public void contextDestroyed(ServletContextEvent arg0) {
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		Driver d = null;
+		while (drivers.hasMoreElements()) {
+			try {
+				d = drivers.nextElement();
+				DriverManager.deregisterDriver(d);
+				logger.warn(String.format("Driver %s deregistered", d));
+			} catch (SQLException ex) {
+				logger.warn(String.format("Error deregistering driver %s", d), ex);
+			}
+		}
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		try {
+			if (BygleSystemUtils.getStringProperty("hibernate.hbm2ddl.auto").equals("create")) {
+				BygleSystemUtils.getPropertiesConfiguration().setProperty("hibernate.hbm2ddl.auto", "update");
+				BygleSystemUtils.getPropertiesConfiguration().save();
+			} else if (BygleSystemUtils.getStringProperty("hibernate.hbm2ddl.auto").equals("drop")) {
 				BygleSystemUtils.getPropertiesConfiguration().setProperty("hibernate.hbm2ddl.auto", "update");
 				BygleSystemUtils.getPropertiesConfiguration().save();
 				WebApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(arg0.getServletContext());
-			    EndPointManager endPointManager =  (EndPointManager)springContext.getBean("endPointManager");
-			    endPointManager.dropEndpoint();
+				EndPointManager endPointManager = (EndPointManager) springContext.getBean("endPointManager");
+				endPointManager.dropEndpoint();
 			}
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-    }
+		}
+	}
 }
